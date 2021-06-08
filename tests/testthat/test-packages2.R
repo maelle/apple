0e68fca470950457e3d55bc1df28287edef3a519
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-06-08 13:09:59 +0200

    add custom skipper

--- tests/testthat/test-packages2.R
+++ tests/testthat/test-packages2.R
@@ -9,3 +9,11 @@ test_that("get_packages2() error", {
   withr::local_envvar("SECRET_PLANET_TOKEN" = "")
   expect_snapshot_error(get_packages2("maelle"))
 })
+
+test_that("get_packages2() works", {
+  skip_if_not_beyonce()
+  vcr::use_cassette("packages2-skip", {
+    pkgs <- get_packages2("maelle")
+  })
+  expect_equal(pkgs[2], "bla")
+})
